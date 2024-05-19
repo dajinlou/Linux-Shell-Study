a44c89f357c3346f1d93bf41472ba0e61c5a6ae1
jjh <jjh@163.com>
2024-05-19 21:27:22 +0800

    添加说明

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Linux-Shell-Study
 Linux Shell编程学习
+
+1 For循环的使用 -----  for_shell.sh
+
---
+++ for_shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+for FRUIT in apple banana peer; do
+    echo "I like $FRUIT"
+done
+
+for file in 'ls';do
+    echo "$file+1"
+done
